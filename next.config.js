96909f13c2c8a135f990b10fa6e45d5b9c3d9658
free-professional-tool <freeprofessionaltool@gmail.com>
2025-07-26 18:25:28 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   experimental: {
     serverComponentsExternalPackages: ['pdf-lib', 'pdfjs-dist']
   },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
+    config.externals = [...(config.externals || []), 'canvas', 'jsdom'];
     return config;
   }
 }
