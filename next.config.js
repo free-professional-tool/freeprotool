db5a3acdff085c4a7be214a0302f04186081ddfc
free-professional-tool <freeprofessionaltool@gmail.com>
2025-07-26 18:22:03 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
+    serverComponentsExternalPackages: ['pdf-lib', 'pdfjs-dist']
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { unoptimized: true },
-};
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
